fefc79f07495b90600be2962744bd48f9164fc95
Atik074 <atiksmats@gmail.com>
2023-02-16 18:12:20 +0600

    color add

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log(23)
+const text =  document.getElementById('text');
+
+document.getElementById('btn-bold').addEventListener('click', function(){
+   
+    text.style.fontWeight = '900'
+})
+
+document.getElementById('italic').addEventListener('click', function(){
+   
+     text.style.fontStyle = "italic";
+ })
+
+ document.getElementById('underline').addEventListener('click', function(){
+   
+     text.style.textDecoration = "underline";
+ })
+ document.getElementById('left').addEventListener('click', function(){
+    text.style.textAlign= "left";
+})
+
+
+ document.getElementById('center').addEventListener('click', function(){
+       text.style.textAlign= "center";
+ })
+ document.getElementById('right').addEventListener('click', function(){
+       text.style.textAlign= "right";
+ })
+    
+
+ document.getElementById('input-color').onchange = function(){
+           text.style.color = this.value;
+ }
+
+
+
